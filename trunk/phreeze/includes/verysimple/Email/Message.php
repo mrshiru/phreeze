798346df4be282cd59414a9b7784276b8e110560
phreezeframework@gmail.com <phreezeframework@gmail.com@ee9e43c2-073d-0410-887a-f3c7bab304f9>
2008-07-30 17:06:16 +0000

    updated mail message to accept multiple addresses

--- trunk/phreeze/includes/verysimple/Email/Message.php
+++ trunk/phreeze/includes/verysimple/Email/Message.php
@@ -36,21 +36,50 @@ class Message
         $this->Format = MESSAGE_FORMAT_TEXT;
     }
 	
+	/**
+	 * Set the sender of the message.  This will appear in some email clients
+	 * as "on behalf of"
+	 * @param string
+	 */
 	function SetSender($email)
 	{
 		$this->Sender = $email;
 	}
 	
+	/**
+	 * Set the from address of the message
+	 * @param string
+	 */
     function SetFrom($email,$name="")
     {
         $this->From = new Recipient($email,$name);
     }
     
+	/**
+	 * Adds a recipient to the email message
+	 * @param $email a single email or semi-colon/comma delimited list
+	 * @param $string a single email or semi-colon/comma delimited list
+	 */
     function AddRecipient($email,$name="")
     {
-        $this->Recipients[] = new Recipient($email,$name);
+		$email = str_replace(",",";",$email);
+		$emails = explode(";",$email);
+
+		$name = str_replace(",",";",$name);
+		$names = explode(";",$name);
+		
+		for ($i = 0; $i < count($emails); $i++)
+		{
+			$addr = trim($emails[$i]);
+			$realname = isset($names[$i]) ? $names[$i] : $addr;
+			$this->Recipients[] = new Recipient( $addr ,$realname);
+		}
     }
     
+	/**
+	 * Attach a message to the email.
+	 * @param the full  path to the file to be attached
+	 */
     function AddAttachment($path)
     {
         $this->Attachments[] = $path;
