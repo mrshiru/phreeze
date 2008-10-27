a2595c03dd652ec802078aa7209d9e9194d7faee
phreezeframework@gmail.com <phreezeframework@gmail.com@ee9e43c2-073d-0410-887a-f3c7bab304f9>
2008-10-27 23:10:54 +0000

    added ExceptionFormatter

---
+++ trunk/phreeze/includes/verysimple/Util/ExceptionFormatter.php
@@ -0,0 +1,50 @@
+<?php
+/** @package    verysimple::Util */
+
+/**
+ * Formatter for formatting Exceptions and stack traces
+ *
+ * @package    verysimple::String
+ * @author Jason Hinkle
+ * @copyright  1997-2008 VerySimple, Inc.
+ * @license    http://www.gnu.org/licenses/lgpl.html  LGPL
+ * @version 1.0
+ */
+class ExceptionFormatter
+{
+	/**
+	* Formats the debug_backtrace array into a printable string
+	*
+	* @access     public
+	* @param array  debug_backtrace array
+	* @param string $join the string used to join the array
+	* @return string
+	*/
+	static function FormatTrace($tb, $join = " :: ", $show_lines = true)
+	{
+		$msg = "";
+		$delim = "";
+		
+		$calling_function = "";
+		$calling_line = "[?]";
+		for ($x = count($tb); $x > 0; $x--)
+		{
+			$stack = $tb[$x-1];
+			$s_file = isset($stack['file']) ? basename($stack['file']) : "[?]";
+			$s_line = isset($stack['line']) ? $stack['line'] : "[?]";
+			$s_function = isset($stack['function']) ? $stack['function'] : "";
+			$s_class = isset($stack['class']) ? $stack['class'] : "";
+			$s_type = isset($stack['type']) ? $stack['type'] : "";
+			
+			$msg .= $delim . "$calling_function" . ($show_lines ? " ($s_file Line $s_line)" : "");
+			$calling_function = $s_class . $s_type . $s_function;
+			
+			$delim = $join;
+		}
+		
+		return $msg;
+		
+	}
+}
+
+?>
