68976277c2c89bd31104a85a91afc5f8eacaa660
phreezeframework <phreezeframework@gmail.com>
2010-10-26 00:10:55 +0000

    better implementation of serialization fixed various issues with 2nd level cache using memcached

--- phreeze/includes/verysimple/Phreeze/CacheMemCache.php
+++ phreeze/includes/verysimple/Phreeze/CacheMemCache.php
@@ -16,29 +16,41 @@ require_once("ICache.php");
 class CacheMemCache implements ICache
 {
 	private $_memcache = null;
+	private $_prefix = "";
 	
 	/**
 	 * Constructor requires a reference to a MemCache object
-	 * @param $memcache
+	 * @param Memcache memcache object
+	 * @param string a unique prefix to use so this app doesn't conflict with any others that may use the same memcache pool
 	 */
-	public function CacheMemCache($memcache)
+	public function CacheMemCache($memcache,$uniquePrefix = "CACHE-")
 	{
 		$this->_memcache = $memcache;
+		$this->_prefix = $uniquePrefix ? $uniquePrefix . "-" : "";
 	}
 	
+	/**
+	 * @inheritdocs
+	 */
 	public function Get($key,$flags=null)
 	{
-		return $this->_memcache->get($key);
+		return $this->_memcache->get($this->_prefix . $key);
 	}
 	
+	/**
+	 * @inheritdocs
+	 */
 	public function Set($key,$val,$flags=null,$timeout=null)
 	{
-		return $this->_memcache->set($key,$val,$flags,$timeout);
+		return $this->_memcache->set($this->_prefix . $key,$val,$flags,$timeout);
 	}
 
+	/**
+	 * @inheritdocs
+	 */
 	public function Delete($key)
 	{
-		return $this->_memcache->delete($key);
+		return $this->_memcache->delete($this->_prefix . $key);
 	}
 	
 }
