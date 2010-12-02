e8e8501a8d7b4b8a77be2b824df85e704c60d898
phreezeframework@gmail.com <phreezeframework@gmail.com@ee9e43c2-073d-0410-887a-f3c7bab304f9>
2010-12-02 19:22:42 +0000

    xml rendering only includes numeric html encoding for more browser compatibility with special characters

--- trunk/phreeze/includes/verysimple/Phreeze/Controller.php
+++ trunk/phreeze/includes/verysimple/Phreeze/Controller.php
@@ -289,7 +289,7 @@ abstract class Controller
 						$val = serialize($val);
 					}
 					
-					$val = VerySimpleStringUtil::EncodeSpecialCharacters($val);
+					$val = VerySimpleStringUtil::EncodeSpecialCharacters($val, true, false, true);
 					
 					// $val = htmlentities(print_r($_REQUEST,1) );
 					$xml .= "<" . htmlspecialchars($var) . ">" . $val . "</" . htmlspecialchars($var) . ">\r\n";
