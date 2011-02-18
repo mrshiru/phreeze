1f67abcbce3da716f7b5b5b37439dd32fe9708c7
phreezeframework <phreezeframework@gmail.com>
2011-02-18 01:11:34 +0000

    added fieldmap type FM_TYPE_YEAR

--- phreeze/includes/verysimple/Phreeze/FieldMap.php
+++ phreeze/includes/verysimple/Phreeze/FieldMap.php
@@ -25,6 +25,8 @@ define("FM_TYPE_TIMESTAMP",17);
 define("FM_TYPE_ENUM",18);
 define("FM_TYPE_TINYTEXT",19);
 
+define("FM_TYPE_YEAR",21);
+
 define("FM_CALCULATION",99); // not to be used during save
 
 /**
