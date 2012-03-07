e2e62425bb03fbe885081a7f49cdb72184185d7e
phreezeframework@gmail.com <phreezeframework@gmail.com@ee9e43c2-073d-0410-887a-f3c7bab304f9>
2012-03-07 21:21:22 +0000

    added timeout param to httprequest

--- trunk/phreeze/includes/verysimple/HTTP/HttpRequest.php
+++ trunk/phreeze/includes/verysimple/HTTP/HttpRequest.php
@@ -12,17 +12,17 @@
  */
 class HttpRequest
 {
-	
+
 	static $METHOD_GET = "GET";
 	static $METHOD_POST = "POST";
 	static $METHOD_PUT = "PUT";
 	static $METHOD_DELETE = "DELETE";
-	
+
 	static $USER_AGENT = "verysimple::HttpRequest";
 	static $VERIFY_CERT = false;
-	
+
 	/**
-	 * 
+	 *
 	 * @param $method
 	 * @param $params
 	 */
@@ -30,7 +30,7 @@ class HttpRequest
 	{
 		$qs = HttpRequest::ArrayToQueryString($params);
 		$ch = null;
-		
+
 		switch ($method)
 		{
 			case HttpRequest::$METHOD_GET:
@@ -44,16 +44,16 @@ class HttpRequest
 			case HttpRequest::$METHOD_PUT:
 				$ch = curl_init($endpoint);
 				// curl_setopt($ch, CURLOPT_PUT, 1); // <- this method requires CURLOPT_INFILE
-				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT'); 
+				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $qs);
 				break;
 			case HttpRequest::$METHOD_DELETE:
 				$ch = curl_init($endpoint);
 				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $qs);
-				break;			
+				break;
 		}
-		
+
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_VERBOSE, 0); // <- ENABLE DEBUGGING
@@ -63,96 +63,97 @@ class HttpRequest
 
 		// make the request
 		$response = curl_exec ($ch);
-		
+
 		// if error is not empty, then a network error occured
 		$error = curl_error($ch);
 		if ($error) {$response .= $error;}
-		
+
 		curl_close ($ch);
-		
+
 		return $response;
 	}
-	
-	/** 
+
+	/**
 	* Make an HTTP POST request using the best method available on the server
-	* 
+	*
 	* @param string $url
 	* @param array $data (array of field/value pairs)
 	* @param bool true to require verification of SSL cert
 	* @return string
 	*/
-	static function Post($url, $data, $verify_cert = false)
+	static function Post($url, $data, $verify_cert = false, $timeout = 30)
 	{
 		if (function_exists("curl_init"))
 		{
-			return HttpRequest::CurlPost($url, $data, $verify_cert);
+			return HttpRequest::CurlPost($url, $data, $verify_cert, $timeout);
 		}
 		else
 		{
-			return HttpRequest::FilePost($url, $data, $verify_cert);
+			return HttpRequest::FilePost($url, $data, $verify_cert, $timeout);
 		}
 	}
-	
-	/** 
+
+	/**
 	* Make an HTTP GET reequest using the best method available on the server
-	* 
+	*
 	* @param string $url
 	* @param array $data (array of field/value pairs)
 	* @param bool true to require verification of SSL cert
+	* @param int timeout (in seconds)
 	* @return string
 	*/
-	static function Get($url, $data = "", $verify_cert = false)
+	static function Get($url, $data = "", $verify_cert = false, $timeout = 30)
 	{
 		if (function_exists("curl_init"))
 		{
-			return HttpRequest::CurlGet($url, $data, $verify_cert);
+			return HttpRequest::CurlGet($url, $data, $verify_cert, $timeout);
 		}
 		else
 		{
-			return HttpRequest::FileGet($url, $data, $verify_cert);
+			return HttpRequest::FileGet($url, $data, $verify_cert, $timeout);
 		}
 	}
-	
-	/** 
+
+	/**
 	 * Make an HTTP GET request using file_get_contents
-	 * 
+	 *
 	 * @param string $url
 	 * @param array $data (array of field/value pairs)
 	 * @param bool true to require verification of SSL cert
 	 * @return string
 	 */
-	static function FileGet($url, $data = "", $verify_cert = false)
+	static function FileGet($url, $data = "", $verify_cert = false, $timeout = 30)
 	{
 		$qs = HttpRequest::ArrayToQueryString($data);
 		$full_url = $url . ($qs ? "?" . $qs : "");
 		return file_get_contents( $full_url );
 	}
-	
-	/** 
+
+	/**
 	 * Make an HTTP POST request using file_get_contents
-	 * 
+	 *
 	 * @param string $url
 	 * @param array $data (array of field/value pairs)
 	 * @param bool true to require verification of SSL cert
 	 * @return string
 	 */
-	static function FilePost($url, $data = "", $verify_cert = false) 
+	static function FilePost($url, $data = "", $verify_cert = false, $timeout = 30)
 	{
 		$qs = HttpRequest::ArrayToQueryString($data);
 		$url = $url . ($qs ? "?" . $qs : "");
-		
+
 		$show_headers = false;
 		$url = parse_url($url);
-		
+
 		if (!isset($url['port'])) {
 			if ($url['scheme'] == 'http') { $url['port']=80; }
 			elseif ($url['scheme'] == 'https') { $url['port']=443; }
 		}
 		$url['query']=isset($url['query'])?$url['query']:'';
-		
+
 		$url['protocol']=$url['scheme'].'://';
 		$eol="\r\n";
-		
+
 		$headers =  "POST ".$url['protocol'].$url['host'].$url['path']." HTTP/1.0".$eol.
 			"Host: ".$url['host'].$eol.
 			"Referer: ".$url['protocol'].$url['host'].$url['path'].$eol.
@@ -160,13 +161,13 @@ class HttpRequest
 			"Content-Length: ".strlen($url['query']).$eol.
 			$eol.$url['query'];
 		$fp = fsockopen($url['host'], $url['port'], $errno, $errstr, 30);
-		if($fp) 
+		if($fp)
 		{
 			fputs($fp, $headers);
 			$result = '';
 			while(!feof($fp)) { $result .= fgets($fp, 128); }
 			fclose($fp);
-			if (!$show_headers) 
+			if (!$show_headers)
 			{
 				//removes headers
 				$match = preg_split("/\r\n\r\n/s",$result,2);
@@ -176,49 +177,49 @@ class HttpRequest
 			return $result;
 		}
 	}
-	
-	/** 
+
+	/**
 	 * Make an HTTP GET request using CURL
-	 * 
+	 *
 	 * @param string $url
 	 * @param variant $data querystring or array of field/value pairs
 	 * @param bool true to require verification of SSL cert
 	 * @return string
 	 */
-	static function CurlGet($url, $data = "", $verify_cert = false) 
+	static function CurlGet($url, $data = "", $verify_cert = false, $timeout = 30)
 	{
-		return HttpRequest::CurlRequest("GET",$url, $data, $verify_cert);
+		return HttpRequest::CurlRequest("GET",$url, $data, $verify_cert, $timeout);
 	}
-	
-	/** 
+
+	/**
 	 * Make an HTTP POST request using CURL
-	 * 
+	 *
 	 * @param string $url
 	 * @param variant $data querystring or array of field/value pairs
 	 * @param bool true to require verification of SSL cert
 	 * @return string
 	 */
-	static function CurlPost($url, $data, $verify_cert = false) 
+	static function CurlPost($url, $data, $verify_cert = false, $timeout = 30)
 	{
-		return HttpRequest::CurlRequest("POST",$url, $data, $verify_cert);
+		return HttpRequest::CurlRequest("POST",$url, $data, $verify_cert, $timeout);
 	}
-	
-	/** 
+
+	/**
 	 * Make an HTTP request using CURL
-	 * 
+	 *
 	 * @param string "POST" or "GET"
 	 * @param string $url
 	 * @param variant $data querystring or array of field/value pairs
 	 * @param bool true to require verification of SSL cert
 	 * @return string
 	 */
-	static function CurlRequest($method, $url, $data, $verify_cert = false) 
+	static function CurlRequest($method, $url, $data, $verify_cert = false, $timeout = 30)
 	{
 		$qs = HttpRequest::ArrayToQueryString($data);
 
 		$agent = "verysimple::HttpRequest";
-		
-		// $header[] = "Accept: text/vnd.wap.wml,*.*";    
+
+		// $header[] = "Accept: text/vnd.wap.wml,*.*";
 
 		if ($method == "POST")
 		{
@@ -231,7 +232,7 @@ class HttpRequest
 			$full_url = $url . ($qs ? "?" . $qs : "");
 			$ch = curl_init($full_url);
 		}
-		
+
 		curl_setopt($ch,		CURLOPT_FOLLOWLOCATION, 1);
 		curl_setopt($ch,		CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch,		CURLOPT_VERBOSE, 0); ########### debug
@@ -241,16 +242,17 @@ class HttpRequest
 		// curl_setopt($ch,	    CURLOPT_HTTPHEADER, $header);
 		// curl_setopt($ch,		CURLOPT_COOKIEJAR, "curl_cookie");
 		// curl_setopt($ch,		CURLOPT_COOKIEFILE, "curl_cookie");
-		
+		curl_setopt($ch,		CURLOPT_TIMEOUT,$timeout);
+
 		$tmp = curl_exec ($ch);
 		$error = curl_error($ch);
-		
+
 		if ($error != "") {$tmp .= $error;}
 		curl_close ($ch);
-		
+
 		return $tmp;
 	}
-	
+
 	/**
 	 * Converts an array into a URL querystring
 	 * @param array key/value pairs
@@ -259,7 +261,7 @@ class HttpRequest
 	static function ArrayToQueryString($arr)
 	{
 		$qs = $arr;
-		
+
 		if (is_array($arr))
 		{
 			// convert the data array into a url querystring
